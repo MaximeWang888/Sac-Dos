4f5605b1fa2fa0f789b9d6d221c9d9f572653842
Maxime Wang <888maximewang@gmail.com>
2021-12-27 22:34:54 +0100

    #comment Modify the best method

--- src/knapsack/solve_knapsack_best.py
+++ src/knapsack/solve_knapsack_best.py
@@ -1,31 +1,28 @@
 def solve_knapsack_best(sack, object_dict):
-    sorted_objects_dict = dict(sorted(object_dict.items(), key=lambda x: x[1][0] / x[1][1]))
-    keys = sorted_objects_dict.keys()
-    val = [value[0] for value in sorted_objects_dict.values()]
-    wt = [weight[1] for weight in sorted_objects_dict.values()]
+    keys = object_dict.keys()
+    val = [value[0] for value in object_dict.values()]
+    wt = [weight[1] for weight in object_dict.values()]
     W = sack.capacity
     M = W
     n = len(val)
     contents_key = {}
     isEmpty = True
 
-    # We initialize the matrix with -1 at first.
-    t = [[-1 for i in range(sack.capacity + 1)] for j in range(n + 1)]
+    # We initialize the matrix with 0 at first.
+    t = [[0 for x in range(W + 1)] for x in range(n + 1)]
 
     print("\n\nWith a maximum capacity of: \t" + str(sack.capacity))
 
-    print("Maximum Value is: \t" + str(getBestValue(sack, wt, val, n, t)))
+    print("Maximum Value is: \t" + str(getBestValue(sack.capacity, wt, val, n, t)))
 
-    print("Selected Items: ")
-
-    while n is not 0:
+    while n != 0:
         if t[n][W] is not t[n - 1][W]:
-            if W - wt[n-1] >= 0:
-                contents_key[list(keys)[n - 1]] = (val[n - 1], wt[n - 1])
-                print("Item : " + str(list(keys)[n - 1]) + " with weight = " +
-                      str(wt[n - 1]) + " and value = " + str(val[n - 1]))
-                W = W - wt[n - 1]
-                isEmpty = False
+            contents_key[list(keys)[n - 1]] = (val[n - 1], wt[n - 1])
+            print("Item : " + str(list(keys)[n - 1]) + " with weight = " +
+                  str(wt[n - 1]) + " and value = " + str(val[n - 1]))
+            isEmpty = False
+
+            W = W - wt[n - 1]
 
         n = n - 1
 
@@ -37,24 +34,15 @@ def solve_knapsack_best(sack, object_dict):
     return sack
 
 
-def getBestValue(sack, wt, val, n, t):
-    W = sack.capacity
-    # base conditions
-    if n == 0 or W == 0:
-        return 0
-    if t[n][W] != -1:
-        return t[n][W]
+def getBestValue(W, wt, val, n, t):
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                t[i][w] = 0
+            elif wt[i - 1] <= w:
+                t[i][w] = max(val[i - 1] + t[i - 1][w - wt[i - 1]], t[i - 1][w])
+            else:
+                t[i][w] = t[i - 1][w]
 
-    # choice diagram code
-    if wt[n - 1] <= W:
-        sack.capacity = sack.capacity - wt[n - 1]
-        t[n][W] = max(
-            val[n - 1] + getBestValue(
-                sack, wt, val, n - 1, t),
-            getBestValue(sack, wt, val, n - 1, t))
-        return t[n][W]
-    elif wt[n - 1] > W:
-        t[n][W] = getBestValue(sack, wt, val, n - 1, t)
-        return t[n][W]
-
-
+    return t[n][W]
